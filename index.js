5b75c5e6c8be3cf679e59c570f2264a31c9c6dcc
Robin Speekenbrink <robin@kingsquare.nl>
2017-03-24 11:02:54 +0100

    updated export

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const buster = require('gulp-buster');
 
-module.exports = buster({
-    fileName: 'cachebuster.php',
-    formatter: function (hashes) {
-        const buffer = [];
-        Object.keys(hashes).sort().forEach((key) => {
-            buffer.push("\t'" + key.replace(/^view\/layout\//, '') + "' => '" + hashes[key] + "',\n");
-        });
-        return '<?php\n/*! GENERATED FILE, DO NOT EDIT */\nreturn [\n' + buffer.join('') + '];';
-    }
-});
+module.exports = () => {
+    return buster({
+        fileName: 'cachebuster.php',
+        formatter: (hashes) => {
+            const buffer = [];
+            Object.keys(hashes).sort().forEach((key) => {
+                buffer.push("\t'" + key.replace(/^view\/layout\//, '') + "' => '" + hashes[key] + "',\n");
+            });
+            return '<?php\n/*! GENERATED FILE, DO NOT EDIT */\nreturn [\n' + buffer.join('') + '];';
+        }
+    });
+}
